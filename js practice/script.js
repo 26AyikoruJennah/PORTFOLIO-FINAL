bed036a7ce97c9911c74ab443a85082a6e6bcc1d
JENNAH AYIKORU <AYKORULOVE@GMAIL.COM>
2022-11-16 04:38:23 +0300

    add portfolio project

---
+++ js practice/script.js
@@ -0,0 +1,3 @@
+console.log(Math.random()); // Print random numbers from 10 to 60
+Math.random() * 100;
+console.log(Math.floor(Math.random() * 100)); //Prints a random whole number between 10 and 60
